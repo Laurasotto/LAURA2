d362249e9b6c0ed2f847d28457c747c6b0286149
Sijita <simonjt.s2001@gmail.com>
2023-08-04 17:42:26 -0500

    Fix Distributor API

--- src/app/api/auth/distributor/route.js
+++ src/app/api/auth/distributor/route.js
@@ -6,54 +6,53 @@ const prisma = new PrismaClient();
 
 //metodo get para obtener todos los distribuidores
 export async function GET() {
-  const distribuidores = await prisma.distribuidor.findMany({
-    include: {
-      distri_negocio: true,
-    },
-  });
-  return new NextResponse(
-    JSON.stringify(distribuidores),
-    {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
+  try {
+    const distribuidores = await prisma.distribuidor.findMany({
+      include: {
+        distri_negocio: true,
       },
-    }
-  );
+    });
+
+    return NextResponse.json(distribuidores);
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
 }
 
-
-
 export async function POST(request) {
-  const { nombre_distribuidor, apellido_distribuidor, telefono_distribuidor, documento_distribuidor, negocios } = await request.json();
+  const {
+    nombre_distribuidor,
+    apellido_distribuidor,
+    telefono_distribuidor,
+    documento_distribuidor,
+    negocios,
+  } = await request.json();
   console.log(nombre_distribuidor);
   console.log(apellido_distribuidor);
   console.log(telefono_distribuidor);
   console.log(documento_distribuidor);
   console.log(negocios);
 
-  const distribuidor = await prisma.distribuidor.create({
-    data: {
-      nombre_distribuidor,
-      apellido_distribuidor,
-      telefono_distribuidor,
-      documento_distribuidor,
-      distri_negocio: {
-        create: negocios.map((negocioId) => ({ negocio: { connect: { id_negocio: negocioId } } })),
+  try {
+    const distribuidor = await prisma.distribuidor.create({
+      data: {
+        nombre_distribuidor,
+        apellido_distribuidor,
+        telefono_distribuidor,
+        documento_distribuidor,
+        distri_negocio: {
+          create: negocios.map((negocioId) => ({
+            negocio: { connect: { id_negocio: negocioId } },
+          })),
+        },
       },
-    },
-    include: {
-      distri_negocio: true,
-    },
-  });
+      include: {
+        distri_negocio: true,
+      },
+    });
 
-  return new NextResponse(
-    JSON.stringify(distribuidor),
-    {
-      status: 201,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+    return NextResponse.json(distribuidor);
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
 }
